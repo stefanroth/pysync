dc8a72915c9ea615a4605e23489d1a8dac067a3b
Stefan Roth <stefan.roth@futuretek.ch>
2018-04-08 15:38:08 +0200

    change dir to script dir and rechange before exiting

--- sync.py
+++ sync.py
@@ -3,11 +3,19 @@
 import yaml
 import sys
 import time
+import os
 from subprocess import call
 from projectconfig import ProjectList
 
 
+def exit_script(calling_folder, message="Exiting.", exit_state=0):
+    print message
+    sys.exit(exit_state)
+
+
 def main():
+    calling_folder = os.getcwd()
+    os.chdir(os.path.dirname(__file__))
     print "Project Synchronisation Script"
     with open("./config/sync.yaml", 'r') as stream:
         try:
@@ -21,7 +29,7 @@ def main():
     if len(sys.argv) < 2:
         print "What project do you want to sync?"
         print "sync <project 1> ... <project n>"
-        print "Exiting."
+        exit_script(calling_folder)
     else:
         all_projects_are_valid = True
         projects_to_sync = []
@@ -30,8 +38,8 @@ def main():
             project = pl.find_by_name(project_name)
 
             if project is None:
-                print "No project named", project_name, "found. Exiting."
-                exit(1)
+                print "No project named", project_name, "found."
+                exit_script(calling_folder, "Exiting.", 1)
 
             if project.validate():
                 projects_to_sync.append(project)
@@ -61,7 +69,7 @@ def main():
                         sys.stdout.write("\033[" + str(len(str(loop))) + "D")
                         sys.stdout.flush()
                     for p in projects_to_sync:
-                        time.sleep(0.5)
+                        time.sleep(0.2)
                         call(p.Command, shell=True)
                         sys.stdout.flush()
                     sys.stdout.write(str(loop))
@@ -73,7 +81,7 @@ def main():
                 print "Exiting after " + str(loop) + " loops."
                 print "Have a nice day!"
                 sys.exit(0)
-        print "Exiting."
+        exit_script(calling_folder)
 
 
 if __name__ == "__main__":
